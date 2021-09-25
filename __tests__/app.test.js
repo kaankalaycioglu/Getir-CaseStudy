798b84eec111c3ccb256687d20257934ff9c7e60
Kaan Kalaycıoğlu <kaankalaycioglu21@gmail.com>
2021-09-25 23:57:25 +0300

    Add new tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,4 +13,24 @@ describe('POST / ', () => {
         expect(response.body.msg).toBe('Success');
         expect(response.body).toHaveProperty('records');
     });
+    it('should respond with an error if any request field is missing', async () => {
+        const response = await request(app).post('/').send({
+        startDate: '2016-01-26',
+        endDate: '2018-02-02',
+        minCount: 2700
+        });
+        expect(response.body.code).toBe(500);
+        expect(response.body.msg).toBe('Missing fields in request payload');
+    });
+
+    it('should respond with an error if request is send to wrong routes', async () => {
+        const response = await request(app).post('/getir').send({
+        startDate: '2016-01-26',
+        endDate: '2018-02-02',
+        minCount: 2700,
+        maxCount: 3000
+        });
+        expect(response.body.code).toBe(404);
+        expect(response.body.msg).toBe('Not Found!');
+    });
   });
